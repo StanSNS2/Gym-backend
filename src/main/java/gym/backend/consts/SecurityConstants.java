54b0ac7f1076ce69e293689ac28ff3084200e143
Stanimir Sergev <stanimir.sergev@dataparkinc.com>
2024-03-19 12:55:51 +0200

    add frontend variable

--- src/main/java/gym/backend/consts/SecurityConstants.java
+++ src/main/java/gym/backend/consts/SecurityConstants.java
@@ -1,5 +1,6 @@
 package gym.backend.consts;
 
 public class SecurityConstants {
-    public static final String FRONTEND_BASE_URL ="https://gym-g99y.onrender.com";
+//    public static final String FRONTEND_BASE_URL ="https://gym-g99y.onrender.com";
+    public static final String FRONTEND_BASE_URL ="${FRONTEND_BASE_URL}";
 }
